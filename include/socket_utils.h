baa8a9ddd14ba9f56c0e5981caa53b361b81bbda
Josef Joubert <josef.joubert@scailable.net>
2023-11-03 11:36:28 +0100

    Added socket utilities files

---
+++ include/socket_utils.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+extern bool sclbl_socket_interrupt_signal;
+
+void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t ) );
+
+void sclbl_socket_send( const char *socket_path, const char *string_to_send, const char *raw_data, size_t raw_data_length );
+
+#ifdef __cplusplus
+}
+#endif
