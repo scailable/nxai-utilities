206d7d7afdcac706fba26756978343907f4c098b
Josef Joubert <jjoubert@networkoptix.com>
2023-11-06 15:00:21 +0100

    Added passing connection fd to callback function

--- include/sclbl_socket_utils.h
+++ include/sclbl_socket_utils.h
@@ -10,7 +10,7 @@ extern "C" {
 
 extern bool sclbl_socket_interrupt_signal;
 
-void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t ) );
+void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t , int) );
 
 void sclbl_socket_send( const char *socket_path, const char *string_to_send, uint32_t message_length );
 
