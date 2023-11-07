19af636e11b3ae5b8bc214ba9df68db42b661bf7
Josef Joubert <josef.joubert@scailable.net>
2023-11-07 02:14:29 +0100

    Dissected listerner function into subfunctions for granular control

--- include/sclbl_socket_utils.h
+++ include/sclbl_socket_utils.h
@@ -10,13 +10,17 @@ extern "C" {
 
 extern bool sclbl_socket_interrupt_signal;
 
-void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t , int) );
+int sclbl_socket_create_listener( const char *socket_path );
+
+int sclbl_socket_await_message( int socket_fd, size_t *allocated_buffer_size, char **message_input_buffer, uint32_t *message_length );
+
+void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t, int ) );
 
 void sclbl_socket_send( const char *socket_path, const char *string_to_send, uint32_t message_length );
 
-char* sclbl_socket_send_receive_message(const char *socket_path, const char *string_to_send, const uint32_t output_message_length);
+char *sclbl_socket_send_receive_message( const char *socket_path, const char *string_to_send, const uint32_t output_message_length );
 
-bool sclbl_socket_send_to_socket(const int socket_fd, const char *message_to_send, uint32_t message_length);
+bool sclbl_socket_send_to_socket( const int socket_fd, const char *message_to_send, uint32_t message_length );
 
 #ifdef __cplusplus
 }
