f8b250f3d88da2d16a6e35bbeabe123858cafbe0
Josef Joubert <jjoubert@networkoptix.com>
2023-11-07 02:14:29 +0100

    Dissected listerner function into subfunctions for granular control

--- src/sclbl_socket_utils.c
+++ src/sclbl_socket_utils.c
@@ -124,27 +124,23 @@ char* sclbl_socket_send_receive_message(const char *socket_path, const char *mes
     return output_string;
 }
 
-/**
- * @brief Listen on socket for incoming messages
- *
- */
-void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t, int ) ) {
+int sclbl_socket_create_listener(const char *socket_path) {
     // Init socket to receive data
     struct sockaddr_un addr;
 
     // Create socket to listen on
     int socket_fd = socket( AF_UNIX, SOCK_STREAM, 0 );
     if ( socket_fd == -1 ) {
-        fprintf( stderr,"Error: Sender socket error. Exiting.\n" );
-        return;
+        fprintf( stderr,"Error: Sender socket error.\n" );
+        return -1;
     }
     if ( strlen( socket_path ) > sizeof( addr.sun_path ) - 1 ) {
-        fprintf( stderr,"Error: Sender socket path too long error. Exiting.\n" );
-        return;
+        fprintf( stderr,"Error: Sender socket path too long error.\n" );
+        return -1;
     }
     if ( remove( socket_path ) == -1 && errno != ENOENT ) {
-        fprintf( stderr,"Error: Sender remove socket error. Exiting.\n" );
-        return;
+        fprintf( stderr,"Error: Sender remove socket error.\n" );
+        return -1;
     }
     memset( &addr, 0, sizeof( struct sockaddr_un ) );
 
@@ -153,69 +149,96 @@ void sclbl_socket_start_listener( const char *socket_path, void ( *callback_func
 
     // Bind to socket
     if ( bind( socket_fd, (struct sockaddr *) &addr, sizeof( struct sockaddr_un ) ) == -1 ) {
-        fprintf(stderr, "Error: Sender socket bind error. Exiting.\n" );
-        return;
+        fprintf(stderr, "Error: Sender socket bind error.\n" );
+        return -1;
     }
 
     // Start listening on socket
     if ( listen( socket_fd, 30 ) == -1 ) {
-        fprintf(stderr, "Error: Sender socket listen error. Exiting.\n" );
-        return;
+        fprintf(stderr, "Error: Sender socket listen error.\n" );
+        return -1;
     }
 
     // Change file permissions so anyone can write to it
     chmod(socket_path, S_IRGRP | S_IRUSR | S_IROTH | S_IWGRP | S_IWOTH | S_IWUSR);
 
+    // Set timeout for socket
+    setsockopt( socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv );
+
+    return socket_fd;
+}
+
+int sclbl_socket_await_message(int socket_fd, size_t *allocated_buffer_size, char** message_input_buffer, uint32_t *message_length) {
+
     size_t num_read_cumulitive = 0;
     ssize_t num_read;
     const int flags = MSG_NOSIGNAL;
+
+    // Wait for incoming connection
+    int connection_fd = accept( socket_fd, NULL, NULL );
+    // Set timeout for socket receive
+
+    // Read message header, which tells us the full message length
+    setsockopt( connection_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv );
+    num_read = recv( connection_fd, message_length, MESSAGE_HEADER_LENGTH, flags );
+    if ( (size_t) num_read != MESSAGE_HEADER_LENGTH ) {
+        return connection_fd;
+    }
+
+    // Allocate space for incoming message.
+    if ( (*message_length) > (*allocated_buffer_size) || (*message_input_buffer) == NULL ) {
+        // Incoming message is larger than allocated buffer. Reallocate.
+        char* new_pointer = realloc((*message_input_buffer),(*message_length) * sizeof( char ));
+        if (new_pointer == NULL) {
+            fprintf(stderr, "Error: Could not allocate buffer with length: %d. Ignoring message.\n",(*message_length));
+            return connection_fd;
+        }
+        // Reallocation succesful
+        *allocated_buffer_size = *message_length;
+        *message_input_buffer = new_pointer;
+    }
+
+    // Read at most read_buffer_size bytes from the socket into read_buffer,
+    // then copy into read_buffer. Reset read count
+    num_read_cumulitive = 0;
+    while ( ( num_read = recv( connection_fd, (*message_input_buffer) + num_read_cumulitive, (*message_length) - num_read_cumulitive, flags ) ) > 0 ) {
+        num_read_cumulitive += (size_t) num_read;
+        if ( num_read_cumulitive >= *message_length ) {
+            // Full message received. Stop reading.
+            break;
+        }
+    }
+    if ( num_read == -1 ) {
+        fprintf(stderr, "Warning: Error when receiving socket message!\n" );
+    }
+
+    return connection_fd;
+}
+
+/**
+ * @brief Listen on socket for incoming messages
+ *
+ */
+void sclbl_socket_start_listener( const char *socket_path, void ( *callback_function )( const char *, uint32_t, int ) ) {
+
+    // Create socket
+    int socket_fd = sclbl_socket_create_listener(socket_path);
+    if (socket_fd == -1){
+        fprintf(stderr, "Error: Failed to create listening socket.\n" );
+        return;
+    }
+
+    uint32_t message_length;
 
     // Buffer for input message
     char *message_input_buffer = NULL;
     size_t allocated_buffer_size = 0;
 
-    uint32_t message_length;
 
     // Listen in a loop
     while ( sclbl_socket_interrupt_signal == 0 ) {
-        // Wait for incoming connection
-        setsockopt( socket_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv );
-        int connection_fd = accept( socket_fd, NULL, NULL );
-        // Set timeout for socket receive
-
-        // Read message header, which tells us the full message length
-        setsockopt( connection_fd, SOL_SOCKET, SO_RCVTIMEO, (const char *) &tv, sizeof tv );
-        num_read = recv( connection_fd, &message_length, MESSAGE_HEADER_LENGTH, flags );
-        if ( (size_t) num_read != MESSAGE_HEADER_LENGTH ) {
-            continue;
-        }
-
-        // Allocate space for incoming message.
-        if ( message_length > allocated_buffer_size ) {
-            // Incoming message is larger than allocated buffer. Reallocate.
-            char* new_pointer = realloc(message_input_buffer,message_length * sizeof( char ));
-            if (new_pointer == NULL) {
-                fprintf(stderr, "Error: Could not allocate buffer with length: %d. Ignoring message.\n",message_length);
-                continue;    
-            }
-            // Reallocation succesful
-            allocated_buffer_size = message_length;
-            message_input_buffer = new_pointer;
-        }
-
-        // Read at most read_buffer_size bytes from the socket into read_buffer,
-        // then copy into read_buffer. Reset read count
-        num_read_cumulitive = 0;
-        while ( ( num_read = recv( connection_fd, message_input_buffer + num_read_cumulitive, message_length - num_read_cumulitive, flags ) ) > 0 ) {
-            num_read_cumulitive += (size_t) num_read;
-            if ( num_read_cumulitive >= message_length ) {
-                // Full message received. Stop reading.
-                break;
-            }
-        }
-        if ( num_read == -1 ) {
-            fprintf(stderr, "Warning: Error when receiving socket message!\n" );
-        }
+        
+        int connection_fd = sclbl_socket_await_message(socket_fd, &allocated_buffer_size, &message_input_buffer, &message_length);
 
         if ( sclbl_socket_interrupt_signal == false ) {
             callback_function( message_input_buffer, message_length, connection_fd );
