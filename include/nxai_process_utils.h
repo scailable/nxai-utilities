d9a47c904f8a8f4e702afe75f68c7e474502f2b6
Josef Joubert <jjoubert@networkoptix.com>
2024-06-25 12:01:44 +0000

    Expanded start process function to allow for log files

--- include/nxai_process_utils.h
+++ include/nxai_process_utils.h
@@ -1,3 +1,4 @@
 #pragma once
+#include <spawn.h>
 
-pid_t nxai_start_process( char *const argv[] );
+pid_t nxai_start_process( char *const argv[], const char* output_filepath, int* output_fd );
--- src/nxai_process_utils.c
+++ src/nxai_process_utils.c
@@ -1,9 +1,18 @@
-#include <spawn.h>
-#include <stdlib.h>
 #include "nxai_process_utils.h"
+#include <fcntl.h>
+#include <spawn.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-pid_t nxai_start_process( char *const argv[] ) {
+pid_t nxai_start_process( char *const argv[], const char *output_filepath, int *output_fd ) {
     pid_t child_pid;
+    *output_fd = open( output_filepath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+    if ( *output_fd == -1 ) {
+        printf( "Warning: Could not open log filepath for process: %s\n", output_filepath );
+        perror( "open" );
+        exit( EXIT_FAILURE );
+    }
 
     char *envp[] = { NULL };
 
@@ -11,6 +20,8 @@ pid_t nxai_start_process( char *const argv[] ) {
     posix_spawn_file_actions_t file_actions;
     posix_spawnattr_t attrp;
     posix_spawn_file_actions_init( &file_actions );
+    posix_spawn_file_actions_adddup2( &file_actions, *output_fd, STDOUT_FILENO );// Redirect STDOUT
+    posix_spawn_file_actions_adddup2( &file_actions, *output_fd, STDERR_FILENO );// Redirect STDERR
     posix_spawnattr_init( &attrp );
 
     // Spawn a new process
