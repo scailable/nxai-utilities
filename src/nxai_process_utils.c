19337c830fd87d1457b0d6e8f32f1b6e5778cc26
Josef Joubert <jjoubert@networkoptix.com>
2025-09-18 10:55:18 +0200

    Fixed bugs where proper log_prefix will not be used

--- src/nxai_process_utils.c
+++ src/nxai_process_utils.c
@@ -386,7 +386,7 @@ static void nxai_vvlog( const char *fmt, va_list *args ) {
 
     if ( _log_to_console == true ) {
         // Print to console
-        printf( "%s%llu %09llu: ", _log_prefix, (uint64_t) timestamp / 1000, duration );
+        printf( "%s%llu %09llu: ", log_prefix, (uint64_t) timestamp / 1000, duration );
         vprintf( fmt, *args );
     }
 
@@ -439,7 +439,7 @@ static void nxai_vvlog( const char *fmt, va_list *args ) {
     }
 
     // Write to logfile
-    int bytes_written = fprintf( flogfile, "%s%llu %09llu: ", _log_prefix, (uint64_t) timestamp / 1000, duration );
+    int bytes_written = fprintf( flogfile, "%s%llu %09llu: ", log_prefix, (uint64_t) timestamp / 1000, duration );
     if ( bytes_written < 0 ) {
         printf( "Failed to write to log file!\n" );
         return;
